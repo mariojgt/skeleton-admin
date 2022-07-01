b589f74bce9bd199861c2a1b521e2d3cf49604c8
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2022-07-01 23:24:14 +0100

    Product update

--- src/Controllers/Backend/Api/Product/ProductController.php
+++ src/Controllers/Backend/Api/Product/ProductController.php
@@ -68,28 +68,34 @@ class ProductController extends Controller
         DB::beginTransaction();
             $product              = new Product();
             $product->name        = Request('name');
+            $product->description = Request('description');
             $product->sku_code    = Request('sku_code');
             $product->category_id = Request('category_id');
-            $product->stock       = Request('stockQty');
+            $product->stock       = Request('stock');
             $product->price       = $money->makePence(Request('price'));
             $product->cost_price  = $money->makePence(Request('cost_price'));
             $product->allergies   = Request('allergies');
             // Cast to boolean
-            $product->use_stock   = Request('useStock') == 'true' ? true : false;
+            $product->use_stock   = Request('use_stock') == 'true' ? true : false;
             $product->is_active   = Request('is_active') == 'true' ? true : false;
             $product->save();
 
-            // Loop through the images and attach them to the product
-            $media  = new MediaController();
-            $folder = MediaFolder::where('name', 'media')->first();
-            foreach (Request('files') as $image) {
-                $newRequest = new Request();
-                $newRequest->merge(['file' => $image]);
-                $file = $media->upload($newRequest, $folder->id);
-                $product->media()->attach($file->getData()->data->id);
+            // Upload the images
+            if (!empty(Request('files'))) {
+                // Loop through the images and attach them to the product
+                $media  = new MediaController();
+                $folder = MediaFolder::where('name', 'media')->first();
+                // Attach the images to the product
+                foreach (Request('files') as $image) {
+                    $newRequest = new Request();
+                    $newRequest->merge(['file' => $image]);
+                    $file = $media->upload($newRequest, $folder->id);
+                    $product->media()->attach($file->getData()->data->id);
+                }
+                // Save the media
+                $product->featured_media_id = $product->media()->first()->id;
             }
-            // Save the media
-            $product->featured_media_id = $product->media()->first()->id;
+
             $product->save();
 
         DB::commit();
