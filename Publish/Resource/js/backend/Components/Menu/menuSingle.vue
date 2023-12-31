4c23b9bf9418f1bc9f835dcb7e3121ab974d7055
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2023-12-31 20:06:56 +0000

    updates in the menu link visit

--- Publish/Resource/js/backend/Components/Menu/menuSingle.vue
+++ Publish/Resource/js/backend/Components/Menu/menuSingle.vue
@@ -1,20 +1,21 @@
 <template>
     <li>
-        <Link :href="routeLink" class="capitalize font-bold">
+        <span class="capitalize" @click="navigateRoute">
         <!-- <component :is="DynamicIcon" /> -->
         {{ props.menuItem.menu_label }}
-        </Link>
+        </span>
     </li>
 </template>
 
-<script setup>
+<script setup lang="ts">
 // Import the inersia script
-import { Link } from "@inertiajs/vue3";
+import { Link, router } from "@inertiajs/vue3";
+import { onMounted } from "vue";
 // Dynamic import the all icons to one variable
 // import * as icons from "@mariojgt/masterui/packages/Icons";
 
 // Store the route link so we can redirect the user
-let routeLink = route(props.menuItem.route);
+let routeLink = null;
 
 // Page props
 const props = defineProps({
@@ -24,7 +25,14 @@ const props = defineProps({
     },
 });
 
+onMounted(() => {
+    routeLink = route(props.menuItem.route);
+})
+
 // Dynamic import the icon component
 // const DynamicIcon = icons[props.menuItem.icon];
+const navigateRoute = async () => {
+    router.get(routeLink);
+};
 
 </script>
