336229d414ca53d8e9b92bd3c64401977e2424b7
Mario Tarosso <mariojgt2@gmail.com>
2025-01-13 20:37:13 +0000

    updates

---
+++ Publish/Public/assets/check-dfbbe6ec.js
@@ -0,0 +1,6 @@
+import{c}from"./createLucideIcon-c54a04d6.js";/**
+ * @license lucide-vue-next v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=c("CheckIcon",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{o as C};
